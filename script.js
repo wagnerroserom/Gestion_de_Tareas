33a3d3c4fed72488f5e74b47c2849c3dffe09cd4
wagnerroserom <wrosero9059@utm.edu.ec>
2025-08-07 11:25:01 -0500

    Se crea la estructura del script insertando la funcionalidad en modo oscuro

---
+++ script.js
@@ -0,0 +1,32 @@
+//Este script se va a cargar en todas las páginas
+document.addEventListener("DOMContentLoaded", function () {
+    
+    // 1. Funcionalidad en modo oscuro
+    const body = document.body;
+    const toggleBtn = document.getElementById("toggleTheme");
+    
+    //Sólo ejecuta si existe el botón en dashboard y add-task
+    if (toggleBtn) {
+    toggleBtn.addEventListener("click", function () {
+      // Alternar la clase dark-mode en el body
+        body.classList.toggle("dark-mode");
+
+      // Cambiar el texto del botón según el estado
+        this.textContent = body.classList.contains("dark-mode")
+        ? "Modo Claro"
+        : "Modo Oscuro";
+    });
+    }
+})
+
+    // 2. Validación
+    const loginForm = document.getElementById("loginForm");
+    if(loginForm) {
+        loginForm.addEventListener("submit", function (e){
+            const usuario = document.getElementById("usuario").value.trim();
+            const contrasena = document.getElementById("contrasena").value.trim();
+            const errorDiv = document.getElementById("errorLogin");
+
+            
+        })
+    }
